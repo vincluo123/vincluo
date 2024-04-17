f9cecbb3f6149da2b13352c0399c7eafef5ec120
Syedmusharaf24 <syedmusharaf2462002@gmaail.com>
2024-04-17 23:48:56 +0530

    headerr

--- src/vinculo/jsfiles/header.js
+++ src/vinculo/jsfiles/header.js
@@ -1,31 +1,42 @@
 import React from 'react';
-import {  FaCartArrowDown, } from 'react-icons/fa';
+import { FaUser , FaShoppingCart } from 'react-icons/fa';
 import '../cssfiles/header.css';
-
-
-
 
 const Header = () => {
   return (
     <header className="header">
-     
       <div className="logo">
-        <img src="logo.png" alt="Logo" />
+        <img src="Hilsa.png" alt="Logo" />
       </div>
 
-     
-
-     
       <nav className="navbar">
         <ul className="nav-menu">
-          <li className="nav-item">
-            <a href="/">Home</a>
-          </li>
-          <li className="nav-item">
-            <a href="/shop">Shop</a>
-          </li>
-          <li className="nav-item">
+        <li className="nav-item">
             <a href="/about">About</a>
+          </li>
+          {/* Dropdown for Home */}
+          <li className="nav-item dropdown">
+            <a href="#" className="dropdown-toggle">Men</a>
+            <ul className="dropdown-menu">
+              <li><a href="#" className="dropdown-item">Submenu 1</a></li>
+              <li><a href="#" className="dropdown-item">Submenu 2</a></li>
+              <li><a href="#" className="dropdown-item">Submenu 3</a></li>
+            </ul>
+          </li>
+          {/* Dropdown for Shop */}
+          <li className="nav-item dropdown">
+            <a href="#" className="dropdown-toggle">Women</a>
+            <ul className="dropdown-menu">
+              <li><a href="#" className="dropdown-item">Category 1</a></li>
+              <li><a href="#" className="dropdown-item">Category 2</a></li>
+              <li><a href="#" className="dropdown-item">Category 3</a></li>
+            </ul>
+          </li>
+          <li className="nav-item">
+            <a href="/contact">Deals</a>
+          </li>
+          <li className="nav-item">
+            <a href="/contact">WishList</a>
           </li>
           <li className="nav-item">
             <a href="/contact">Contact</a>
@@ -34,39 +45,34 @@ const Header = () => {
       </nav>
 
      
+
       <div className="search-bar">
         <input type="text" placeholder="Search..." />
         <button type="submit">Search</button>
       </div>
 
-     
-
-     
       <div className="account-links">
         <a href="/login">Login</a>
         <a href="/register">Register</a>
       </div>
 
-     
-      <div className="shopping-cart">
+      <div className="User">
         <a href="/cart">
-          <FaCartArrowDown />
+          <FaUser />
         </a>
       </div>
-
-     
+      <div className="shopping-cart">
+        <a href="/cart">
+          <FaShoppingCart />
+        </a>
+      </div>
       <div className="language-selector">
         <select>
           <option value="en">English</option>
           <option value="fr">Tamil</option>
           <option value="hi">Hindi</option>
-         
         </select>
       </div>
-
-     
-
-     
     </header>
   );
 };
