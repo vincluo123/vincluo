304096f1535309653e7a16704ab5dd02c887d37d
Syedmusharaf24 <syedmusharaf2462002@gmaail.com>
2024-04-17 22:44:55 +0530

    head

--- src/vinculo/jsfiles/header.js
+++ src/vinculo/jsfiles/header.js
@@ -1,23 +1,72 @@
 import React from 'react';
-import { FaUser, FaCartArrowDown} from 'react-icons/fa';
-import "../cssfiles/header.css"
+import {  FaCartArrowDown, } from 'react-icons/fa';
+import '../cssfiles/header.css';
+
+
+
 
 const Header = () => {
   return (
     <header className="header">
+     
       <div className="logo">
-        <img src="/logo.png" alt="E-commerce Logo" />
+        <img src="logo.png" alt="Logo" />
       </div>
-      <nav className="nav">
-        <ul className="nav__list">
-          <li className="nav__item"><a href="/">Home</a></li>
-          <li className="nav__item"><a href="/products">Products</a></li>
-         
-          
-          <li className="nav__item">  <a href="http" className='icon'><FaUser /></a></li>
-          <li className="nav__item">  <a href="https://instagram.com" className='icon'><FaCartArrowDown /></a></li>
+
+     
+
+     
+      <nav className="navbar">
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <a href="/">Home</a>
+          </li>
+          <li className="nav-item">
+            <a href="/shop">Shop</a>
+          </li>
+          <li className="nav-item">
+            <a href="/about">About</a>
+          </li>
+          <li className="nav-item">
+            <a href="/contact">Contact</a>
+          </li>
         </ul>
       </nav>
+
+     
+      <div className="search-bar">
+        <input type="text" placeholder="Search..." />
+        <button type="submit">Search</button>
+      </div>
+
+     
+
+     
+      <div className="account-links">
+        <a href="/login">Login</a>
+        <a href="/register">Register</a>
+      </div>
+
+     
+      <div className="shopping-cart">
+        <a href="/cart">
+          <FaCartArrowDown />
+        </a>
+      </div>
+
+     
+      <div className="language-selector">
+        <select>
+          <option value="en">English</option>
+          <option value="fr">Tamil</option>
+          <option value="hi">Hindi</option>
+         
+        </select>
+      </div>
+
+     
+
+     
     </header>
   );
 };
