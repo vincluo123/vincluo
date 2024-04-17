f47eb8bdeec6432f1904c22653363909b1aebc82
Karthikeyan <137636670+karthi049@users.noreply.github.com>
2024-04-17 22:23:52 +0530

    summa

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@
 import './App.css';
 import Kids from './vinculo/jsfiles/kids';
 import Home from './vinculo/jsfiles/homepage';
+import CartPage from './vinculo/jsfiles/cart';
+
 
 function App() {
   return (
     <div>
       <Home />
       <Kids />
+      <CartPage />
+
     
 
 
