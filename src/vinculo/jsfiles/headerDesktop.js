034bd362ff98b1b7297ef392b22d5293679e231a
vijayvs <vijayvs6383@gmail.com>
2024-04-24 16:27:09 +0530

    headerDesktop

--- src/vinculo/jsfiles/headerDesktop.js
+++ src/vinculo/jsfiles/headerDesktop.js
@@ -1,7 +1,5 @@
 import '../cssfiles/headerDesktop.css';
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 
 export const Header = () => {
@@ -17,21 +15,22 @@ export const Header = () => {
             <li><a href="#">Infant</a></li>
             <li><a href="#">Boys</a></li>
             <li><a href="#">Girls</a></li>
+            <li><a href="#">Offers</a></li>
           </ul>
-          
-          
 
         </nav>
 
-        <div className="search-bar">
-          <input type="text" />
-          <FontAwesomeIcon className='search-icon' icon={faSearch}  size='2x'/>
+        <div className='right-side-header'>
+          <div className="search-bar">
+            <input type="text" placeholder='Search' />
+            <img src="searchicon.png" alt="" />
+          </div>
+          <button className='login-button'>Login</button>
         </div>
-
 
       </header>
     </>
   )
 }
 
-export default Header
+export default Header
