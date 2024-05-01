fc33ceea55b6e5c2eedbaf662f94bc8beb8c9e53
Syedmusharaf24 <syedmusharaf2462002@gmaail.com>
2024-05-01 20:34:17 +0530

    about

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Header from './vinculo/jsfiles/headerDesktop';
 import CartPage from './vinculo/jsfiles/cart';
 import Footer from './vinculo/jsfiles/footerDesktop';
 import Home from './vinculo/jsfiles/homepageDesktop';
-import ProductDetails from './vinculo/jsfiles/productdetails';
+import Girls from './vinculo/jsfiles/8-16girls';
+//import ProductDetails from './vinculo/jsfiles/productdetails';
 //import OrderConfirmation from './vinculo/jsfiles/orderConfirmationPage';
 // import ProductDetails from './vinculo/jsfiles/productdetails';
 //import Login from './Login';
@@ -22,6 +23,7 @@ function App() {
       <Home/>
       {/* <CartPage/> */}
      {/*<Wishlist /> */}
+     <Girls />
       <Footer/>
       {/* <ProductDetails/> */}
 
