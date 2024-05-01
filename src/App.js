7e4d240cf4d997ebd214978d23265de2d9b22fcb
KAVIYAJAWAHAR <kaviyajawahar03@gmail.com>
2024-05-01 22:19:04 +0530

    about

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import CartPage from './vinculo/jsfiles/cart';
 import Footer from './vinculo/jsfiles/footerDesktop';
 import Home from './vinculo/jsfiles/homepageDesktop';
 import Girls from './vinculo/jsfiles/8-16girls';
+import About from './vinculo/jsfiles/about';
 //import ProductDetails from './vinculo/jsfiles/productdetails';
 //import OrderConfirmation from './vinculo/jsfiles/orderConfirmationPage';
 // import ProductDetails from './vinculo/jsfiles/productdetails';
 //import Login from './Login';
-//import Wishlist from './vinculo/jsfiles/wishlist';
+import Wishlist from './vinculo/jsfiles/wishlist';
 //import Infantboys from './vinculo/jsfiles/Infantboys';
 
 
@@ -22,8 +23,9 @@ function App() {
       <Header/>
       <Home/>
       {/* <CartPage/> */}
-     {/*<Wishlist /> */}
+     <Wishlist /> 
      <Girls />
+     <About/>
       <Footer/>
       {/* <ProductDetails/> */}
 
