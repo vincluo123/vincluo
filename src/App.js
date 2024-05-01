6c767afaaa4e4a5584a528609a86edb23c08ac4c
vijayvs <vijayvs6383@gmail.com>
2024-05-01 12:53:01 +0530

    AccountPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
-import Header from './vinculo/jsfiles/headerDesktop';
-import CartPage from './vinculo/jsfiles/cart';
-import Footer from './vinculo/jsfiles/footerDesktop';
-import Home from './vinculo/jsfiles/homepageDesktop';
-import ProductDetails from './vinculo/jsfiles/productdetails';
+import AccountPage from './vinculo/jsfiles/AccountPage';
+// import Header from './vinculo/jsfiles/headerDesktop';
+// import CartPage from './vinculo/jsfiles/cart';
+// import Footer from './vinculo/jsfiles/footerDesktop';
+// import Home from './vinculo/jsfiles/homepageDesktop';
+// import ProductDetails from './vinculo/jsfiles/productdetails';
 //import OrderConfirmation from './vinculo/jsfiles/orderConfirmationPage';
 // import ProductDetails from './vinculo/jsfiles/productdetails';
 //import Login from './Login';
@@ -20,7 +21,8 @@ function App() {
       <CartPage/> */}
      {/* <OrderConfirmation/> */}
       {/* <Footer/> */}
-      <ProductDetails/>
+      {/* <ProductDetails/> */}
+      <AccountPage />
     </>
   );
 }
