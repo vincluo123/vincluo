8ba575c54a5f3cd836bc46cf583157de27eb673d
Umarfarook1912 <ujuj2451@gmail.com>
2024-05-21 11:52:32 +0530

    sagar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import NotificationPage from "./vinculo/jsfiles/Notification";
 import Faq from "./vinculo/jsfiles/faq";
+import Header from "./vinculo/jsfiles/headerDesktop";
+import HeaderMobile from "./vinculo/mobilehome/headerMobile";
+
 function App() {
   return (
     <>
-    <Faq/>
-    <NotificationPage/>
+    {/* <HeaderMobile/> */}
+    <Header/>
     </>
   );
 }
