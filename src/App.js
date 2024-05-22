4a4e526a6e1397da44dd70fd822a878b131b103d
vidhyasagar70 <vidhyas369@gmail.com>
2024-05-22 11:58:38 +0530

    aboutus

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 //import NotificationPage from "./vinculo/jsfiles/Notification";
 //import Faq from "./vinculo/jsfiles/faq";
-import Header from './vinculo/mobilehome/headerMobile';
-import HeaderDesktop from "./vinculo/jsfiles/headerDesktop";
+// import Header from './vinculo/mobilehome/headerMobile';
+
+import AboutUs from "./vinculo/jsfiles/about";
+
+// import HeaderDesktop from "./vinculo/jsfiles/headerDesktop";
 function App() {
   return (
     <>
-      
-    <Header/>
+      <AboutUs/>
+    
     </>
   );
 }
