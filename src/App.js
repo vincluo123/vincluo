b0e60c809bf20228ea680e8a1a76eba07a12a18b
vidhyasagar70 <vidhyas369@gmail.com>
2024-05-22 10:46:49 +0530

    command

--- src/App.js
+++ src/App.js
@@ -5,108 +5,29 @@ import CartPage from './vinculo/jsfiles/cart';
 import Footer from './vinculo/jsfiles/footerDesktop';
 import Home from './vinculo/jsfiles/homepageDesktop';
 import OrderConfirmation from './vinculo/jsfiles/orderConfirmationPage';
-// import ProductDetails from './vinculo/jsfiles/productdetails';
-//import Login from './Login';
-// import Infantgirls from './vinculo/jsfiles/infantgirls';
-// import Wishlist from './vinculo/jsfiles/wishlist';
 import Faq from './vinculo/jsfiles/faq';
-
 import WishlistPage from './vinculo/jsfiles/wishlist';
-//import Faq from './vinculo/jsfiles/faq';
-//import AboutUs from "./vinculo/jsfiles/about";
-//import Header from "./vinculo/mobilehome/headerMobile";
-// import HeaderMobile from "./vinculo/mobilehome/headerMobile";
-//import Footer from './vinculo/jsfiles/footerDesktop';
-//import AddressForm from './vinculo/jsfiles/AddressForm';
-//import {  Route, Routes , Router } from 'react-router-dom';
-//import AddressForm from './vinculo/jsfiles/AddressForm';
-//import NewAddressForm from './vinculo/jsfiles/NewAddressForm';
-//import Brandnameheader from './vinculo/jsfiles/brandnameheader';
-//import AddressForm from './vinculo/jsfiles/AddressForm';]
-
 import Account from './vinculo/Authentication/AccountPage';
-
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
 function App() {
   const [showAccountPage, setShowAccountPage] = useState(true);
 
   const hideAccountPage = () => {
     setShowAccountPage(false);
   };
-
-  return (
-
-    //     <>
-    //       {/* <BrowserRouter>
-    // <Routes>
-    // <Route path="" element={HeaderMobile}/>
-    // <Route path="/cart" element={CartPage }/>
-
-
-
-    // </Routes>
-
-    // </BrowserRouter> */}
-
-    //       {/*<Router>
-    //       <Routes>
-    //         <Route path="/" element={<AddressForm />} />
-    //         <Route path="/new-address" element={<NewAddressForm />} />
-    //       </Routes>
-    //     </Router>*/}
-
-    //       {/* <HeaderMobile/> */}
-
-
-
-    //       {/* <Wishlist />  */}
-
-    //       {/*<NewAddressForm/> */}
-    //       {/*<AddressForm/> */}
-    //       {/* <Footer/> */}
-
-    //       {/*<CartPage/> */}
-
-    //       {/*<Faq /> */}
-    //       {/* <About/>*/}
-    //       {/*<Wishlist /> */}
-
-    //       {/* <AboutUs/> */}
-    //       {/* <Wishlist />  */}
-    //       {/*<Faq /> */}
-    //       {/* <About/>*/}
-    //       {/*<Footer/> */}
-    //       {/* <ProductDetails/> */}
-    //       {/* <Faq /> */}
-    //       {/*<Offer/> */}
-
-    //       {/*<Kids/>*/}
-
-
-
-
-
-    //       {/* <Header/>*/}
-
-
-    //       {/* <Footer/> */}
-    //       {/* <ProductDetails/> */}
-
-    //     </>
-    
-    <Router>
+return (
+<Router>
       <Header />
       <>
         <>
-          {/* Render the AccountPage component if showAccountPage is true */}
-          {showAccountPage && (
+        
+        {showAccountPage && (
             <Routes>
               <Route path="/account" element={<Account />} />
             </Routes>
           )}
 
-          {/* Define routes */}
+          
           <Routes>
             {/* Route for the Home page */}
             <Route path="/" element={<Home hideAccountPage={hideAccountPage} />} />
