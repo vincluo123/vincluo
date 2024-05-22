931e012f1590f1a1ce44728bb85291676c89c120
vijayvs <vijayvs6383@gmail.com>
2024-05-22 12:20:08 +0530

    hii

--- src/App.js
+++ src/App.js
@@ -36,11 +36,11 @@
 //import Brandnameheader from './vinculo/jsfiles/brandnameheader';
 //import AddressForm from './vinculo/jsfiles/AddressForm';
 //import { BrowserRouter ,Routes,Route} from 'react-router-dom';
-import Smallboys from "./vinculo/jsfiles/Smallboys";
+import Smallgirls from "./vinculo/jsfiles/Smallgirls";
 function App() {
   return (
     <>
-    <Smallboys/>
+    <Smallgirls/>
     </>
   );
 }
