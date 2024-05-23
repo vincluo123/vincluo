ff3af2454a920a00f43f86625043b607d14421da
vidhyasagar70 <vidhyas369@gmail.com>
2024-05-23 17:52:05 +0530

    route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HeaderMobile from './vinculo/mobilehome/headerMobile';
-import ProductDetails from './vinculo/mobilehome/productDetails';
+//import ProductDetails from './vinculo/mobilehome/productDetails';
 
 function App() {
     return (
