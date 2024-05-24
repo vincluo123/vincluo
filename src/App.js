9503561f1fcf14ee68fe9996e5bd9976db9f4c26
vidhyasagar70 <vidhyas369@gmail.com>
2024-05-24 19:38:02 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,40 +1,14 @@
-//import './App.css';
-// import Header from './vinculo/jsfiles/headerDesktop';
-
-// import CartPage from './vinculo/jsfiles/cart';
-
-// import Home from './vinculo/jsfiles/homepageDesktop';
-// import ProductDetails from './vinculo/jsfiles/productdetails';
 
 
-//import CartPage from './vinculo/jsfiles/cart';
-// import Footer from './vinculo/jsfiles/footerDesktop';
+
+
  import CartPage from './vinculo/jsfiles/cart';
-//  import Footer from './vinculo/jsfiles/footerDesktop';
-// import Home from './vinculo/jsfiles/homepageDesktop';
-// import ProductDetails from './vinculo/jsfiles/productdetails';
-//import Kids from './vinculo/jsfiles/kids';
-//import Offer from './vinculo/jsfiles/offer';
 
-//import OrderConfirmation from './vinculo/jsfiles/orderConfirmationPage';
-// import ProductDetails from './vinculo/jsfiles/productdetails';
-//import Login from './Login';
+
 import Infantgirls from './vinculo/jsfiles/infantgirls';
-// import Wishlist from './vinculo/jsfiles/wishlist';
-// import Faq from './vinculo/jsfiles/faq';
 
-//import Wishlist from './vinculo/jsfiles/wishlist';
-//import Faq from './vinculo/jsfiles/faq';
-//import AboutUs from "./vinculo/jsfiles/about";
-//import Header from "./vinculo/mobilehome/headerMobile";
 import HeaderMobile from "./vinculo/mobilehome/headerMobile";
-//import Footer from './vinculo/jsfiles/footerDesktop';
-//import AddressForm from './vinculo/jsfiles/AddressForm';
-//import {  Route, Routes , Router } from 'react-router-dom';
-//import AddressForm from './vinculo/jsfiles/AddressForm';
-//import NewAddressForm from './vinculo/jsfiles/NewAddressForm';
-//import Brandnameheader from './vinculo/jsfiles/brandnameheader';
-//import AddressForm from './vinculo/jsfiles/AddressForm';
+
 import { BrowserRouter ,Routes,Route} from 'react-router-dom';
 
 function App() {
