68778e52d04e62bb06639fdf0b7bd2f1e149355f
vidhyasagar70 <vidhyas369@gmail.com>
2024-05-24 17:38:41 +0530

    home

--- src/App.js
+++ src/App.js
@@ -1,44 +1,5 @@
-//import './App.css';
-// import Header from './vinculo/jsfiles/headerDesktop';
 
-// import CartPage from './vinculo/jsfiles/cart';
-
-// import Home from './vinculo/jsfiles/homepageDesktop';
-// import ProductDetails from './vinculo/jsfiles/productdetails';
-
-
-//import CartPage from './vinculo/jsfiles/cart';
-// import Footer from './vinculo/jsfiles/footerDesktop';
-// import CartPage from './vinculo/jsfiles/cart';
-//  import Footer from './vinculo/jsfiles/footerDesktop';
-// import Home from './vinculo/jsfiles/homepageDesktop';
-// import ProductDetails from './vinculo/jsfiles/productdetails';
-<<<<<<< HEAD
-import Kids from './vinculo/jsfiles/kids';
-//import Offer from './vinculo/jsfiles/offer';
-=======
-// import Kids from './vinculo/jsfiles/kids';
-import Offer from './vinculo/jsfiles/offer';
->>>>>>> 64313bf3b977053d82cd152b4c26c57398dd4959
-
-//import OrderConfirmation from './vinculo/jsfiles/orderConfirmationPage';
-// import ProductDetails from './vinculo/jsfiles/productdetails';
-//import Login from './Login';
-
-// import Wishlist from './vinculo/jsfiles/wishlist';
-// import Faq from './vinculo/jsfiles/faq';
-
-//import Wishlist from './vinculo/jsfiles/wishlist';
-//import Faq from './vinculo/jsfiles/faq';
-//import AboutUs from "./vinculo/jsfiles/about";
-
-//import HeaderMobile from "./vinculo/jsfiles/headerMobile";
-// import Footer from './vinculo/jsfiles/footerDesktop';
-
-//import Brandnameheader from './vinculo/jsfiles/brandnameheader';
-
-
-
+import HomepageDesktop from './vinculo/jsfiles/homepageDesktop'
 
 function App() {
   return (
@@ -53,7 +14,7 @@ function App() {
     {/*<Faq /> */}
      {/* <About/>*/}
     {/*<Wishlist /> */}
-    <Kids/>
+   
      {/* <AboutUs/> */} 
       {/* <Wishlist />  */}
       {/*<Faq /> */}
@@ -61,11 +22,11 @@ function App() {
       {/* <Footer/> */}
       {/* <ProductDetails/> */}
     {/* <Faq /> */}
-     <Offer/> 
+      
 
 
 
-
+<HomepageDesktop/>
 
 
 
